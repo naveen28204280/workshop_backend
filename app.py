7a3772b503b1236abcbadd6371b64478151be614
Naveen <naveensrinivas282@gmail.com>
2025-07-06 14:04:00 +0530

    create_order as per cashfree

--- app.py
+++ app.py
@@ -21,45 +21,51 @@ Cashfree.XClientSecret = os.getenv("CASHFREE_API_KEY")
 Cashfree.XEnvironment = Cashfree.XSandbox
 x_api_version = "2023-08-01"
 
+
 class PaymentDetails(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
-    name = db.Column(db.String, nullable = False)
-    email = db.Column(db.String, unique=True, nullable = False)
-    transaction_id = db.Column(db.Integer, unique=True, nullable = False)
-    roll_no = db.Column(db.String, unique = True, nullable = False)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    phone_number = db.Column(db.Integer, unique=True, nullable=False)
+    transaction_id = db.Column(db.Integer, unique=True, nullable=True)
+    roll_no = db.Column(db.String, unique=True, nullable=False)
+    paid = db.Column(db.Boolean, nullable=False, default=False)
+
 
 with app.app_context():
     db.create_all()
 
-def add_to_DB(name, roll_no, email, transaction_id):
-    student = PaymentDetails(name = name, roll_no = roll_no, email = email, transaction_id = transaction_id)
-    db.add(student)
-    db.commit()
+
+def add_to_DB(name, roll_no, email, phone_number):
+    student = PaymentDetails(
+        name=name, roll_no=roll_no, email=email, phone_number=phone_number
+    )
+    db.session.add(student)
+    db.session.commit()
+    return student.id
+
+
+def confirm_payment(id, transaction_id):
+    student = PaymentDetails.query.get(id=id)
+    student.transaction_id = transaction_id
+    student.paid = True
+    db.session.commit()
     return True
+
 
 def check_prev(name, roll_no, email):
     transaction = PaymentDetails.query.filter(
-        or_(PaymentDetails.name == name, PaymentDetails.roll_no == roll_no, PaymentDetails.email == email)
-        ).first()
+        or_(
+            PaymentDetails.name == name,
+            PaymentDetails.roll_no == roll_no,
+            PaymentDetails.email == email,
+        )
+    ).first()
     if transaction:
         return True
     else:
         return False
 
-def register():
-    try:
-        global seats_left
-        data = request.json
-        if not all(data.get(field) for field in ["name", "roll_no", "email", "transaction_id"]):
-            return jsonify({"error": "Missing required fields"}), 400
-        paid = check_prev(roll_no = data['roll_no'], email = data['email'], name = data['name'])
-        if paid:
-            return ({'error': 'You have already paid'}), 409
-        add_to_DB(name=data['name'], roll_no=data['roll_no'], email=data['email'], transaction_id=data['transaction_id'])
-        seats_left -= 1
-        return jsonify({'success': 'registered'}), 200
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
 
 @app.route("/seats-left/", methods=["GET"])
 def no_of_seats_left():
@@ -68,21 +74,50 @@ def no_of_seats_left():
         return jsonify({"seat_left": seats_left}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-    
-@app.route("/create_order/", methods = ["POST"])
-def create_order():
-    customerDetails = CustomerDetails(customer_id="123", customer_phone="9999999999")
-    createOrderRequest = CreateOrderRequest(order_amount=1, order_currency="INR", customer_details=customerDetails)
-    try:
-        api_response = Cashfree().PGCreateOrder(x_api_version, createOrderRequest, None, None)
-        print(api_response.data)
-        return jsonify({}), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
 
-@app.route(os.getenv("WEBHOOK_PATH"), methods = ["POST"])
+
+@app.route("/create_order/", methods=["POST"])
+def create_order():
+    data = request.json
+    if not all(
+        [
+            data.get("name"),
+            data.get("roll_no"),
+            data.get("email"),
+            data.get("phone_number"),
+        ]
+    ):
+        return jsonify({"error": "Missing required fields"}), 400
+    check_prev(name=data["name"], roll_no=data["roll_no"], email=data["email"])
+    if check_prev:
+        return jsonify({"error": "This details already exist"}), 409
+    id = add_to_DB(
+        name=data["name"], roll_no=data["roll_no"], phone_number=data["phone_number"], email = data['email']
+    )
+    customerDetails = CustomerDetails(
+        customer_id=id, customer_phone=data["phone_number"]
+    )
+    createOrderRequest = CreateOrderRequest(
+        order_amount=1800, order_currency="INR", customer_details=customerDetails
+    )
+    try:
+        api_response = Cashfree().PGCreateOrder(
+            x_api_version, createOrderRequest, None, None
+        )
+        return jsonify(
+            {
+                "order_id": api_response.data["order_id"],
+                "payment_session_id": api_response.data["payment_session_id"],
+            }
+        ), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+@app.route("/payment-confirmation/", methods=["POST"])
 def payment_confirm():
-    pass # calls register if finished
+    pass  # calls register if finished
+
 
 if __name__ == "__main__":
     app.run(debug=True)
