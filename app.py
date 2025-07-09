5a86f3a048c6e4f59f2c3bdff9bfbb3093f17b17
Naveen <naveensrinivas282@gmail.com>
2025-07-09 12:48:35 +0530

    imporve error handling

--- app.py
+++ app.py
@@ -1,21 +1,13 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import or_
+from flask_sqlalchemy import SQLAlchemy, or_
 from dotenv import load_dotenv
 import os
 import requests
 from googleapiclient.errors import HttpError
-from google.oauth2.service_account import Credentials as ServiceAccountCredentials
+from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from email.message import EmailMessage
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-import base64
-import os.path
+
 load_dotenv()
 
 app = Flask(__name__)
@@ -39,39 +31,6 @@ class PaymentDetails(db.Model):
 with app.app_context():
     db.create_all()
 
-def sendMail(to_email: str, subject: str, body: str):
-    SCOPES = ['https://www.googleapis.com/auth/gmail.send']
-    creds = None
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-
-    try:
-        service = build('gmail', 'v1', credentials=creds)
-        message = EmailMessage()
-        message.set_content(body)
-        message['To'] = to_email
-        message['From'] = 'me'
-        message['Subject'] = subject
-
-        encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
-        create_message = {'raw': encoded_message}
-
-        send_message = service.users().messages().send(userId="me", body=create_message).execute()
-        print(f"✅ Email sent successfully to {to_email} (ID: {send_message['id']})")
-        return send_message['id']
-    except Exception as e:
-        print(f"❌ Failed to send email: {e}")
-        return None
-
-
 def add_to_DB(name, roll_no, email, phone_number):
     student = PaymentDetails(
         name=name, roll_no=roll_no, email=email, phone_number=phone_number
@@ -85,7 +44,7 @@ def add_to_sheet(id, name, roll_no, email, phone_number, transaction_id): # adde
         global spreadsheet_id
         range = "A2:F"
         scopes = ["https://www.googleapis.com/auth/spreadsheets"]
-        creds = ServiceAccountCredentials.from_service_account_file("creds.json", scopes=scopes)
+        creds = Credentials.from_service_account_file("creds.json", scopes=scopes)
         service = build("sheets", "v4", credentials=creds)
         values = [[id, name, roll_no, email, phone_number, transaction_id]]
         
@@ -113,14 +72,18 @@ def confirm_payment(id, transaction_id):
     student = PaymentDetails.query.get(id=id)
     student.transaction_id = transaction_id
     db.session.commit()
-    add_to_sheet(
-        id=student.id, 
-        name=student.name,
-        roll_no=student.roll_no,
-        email=student.email, 
-        transaction_id=transaction_id, 
-        phone_number=student.phone_number
-    ) # verify it worked
+    try:
+        add_to_sheet(
+            id=student.id, 
+            name=student.name,
+            roll_no=student.roll_no,
+            email=student.email, 
+            transaction_id=transaction_id, 
+            phone_number=student.phone_number
+        ) # verify it worked
+    except Exception as e:
+        print(f'adding to sheet failed {e}')
+        return False
     return True
 
 def check_prev(name, roll_no, email):
@@ -146,7 +109,7 @@ def no_of_seats_left():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-@app.route("/create_order/", methods=["POST"]) # check add_to_sheet if it throws an error
+@app.route("/create_order/", methods=["POST"])
 def create_order():
     global max_seats
     booked = PaymentDetails.query.filter(PaymentDetails.transaction_id is not None).count()
@@ -168,20 +131,6 @@ def create_order():
     id = add_to_DB(
         name=data["name"], roll_no=data["roll_no"], phone_number=data["phone_number"], email = data['email']
     )
-    # add_to_sheet(
-    #     id=1, 
-    #     name=data["name"],
-    #     roll_no=data["roll_no"],
-    #     email=data["email"], 
-    #     transaction_id=1, 
-    #     phone_number=data["phone_number"]
-    #     )
-    # sendMail(
-    #     to_email=data["email"],
-    #     subject="Workshop Seat Confirmed 🎉",
-    #     body=f"Hi {data["name"]},\n\nYour seat has been confirmed! ✅\n\nTransaction ID: {1}\n\nThank you!"
-    # )
-
     access_token = get_access_token()
     url = "https://api-preprod.phonepe.com/apis/pg-sandbox/checkout/v2/pay"  # change to https://api.phonepe.com/apis/pg/checkout/v2/pay in prod
     headers = {
@@ -203,7 +152,8 @@ def create_order():
     }
     try:
         response = requests.post(url, headers=headers, json=body)
-        return jsonify(response.json()), response.status_code
+        data = response.json
+        return jsonify({'redirectUrl': data['redirectUrl']}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
@@ -211,11 +161,13 @@ def create_order():
 def payment_confirm():
     data = request.json
     if data["payload"]["state"] == "COMPLETED":
-        confirm_payment(
+        if confirm_payment(
             id = data["payload"]["merchantOrderId"], 
             transaction_id=data["payload"]["paymentDetails"]["transactionId"]
-        )
-        return jsonify({'success': 'Booked a seat'}), 200
+        ):
+            return jsonify({'success': 'Booked a seat'}), 200
+        else:
+            return jsonify({'error': 'payment successful but failed to add to sheets'}), 
     else:
         return jsonify({'error': "payment unsuccessfull"}), 400
 
